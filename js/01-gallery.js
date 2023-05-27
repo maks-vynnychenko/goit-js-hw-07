3ab1961b3b72236f297a41548601c773319eafaf
maks-vynnychenko <vinnichenkomaks@gmail.com>
2023-05-27 18:33:36 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+const galerryMarkup = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+);
+
+gallery.insertAdjacentHTML('beforeend', galerryMarkup.join(''));
+gallery.addEventListener('click', onClick);
+
+function onClick(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  event.preventDefault();
+
+  const onCloseModal = event => {
+    const ESC_KEY = 'Escape';
+
+    if (event.code === ESC_KEY) {
+      instance.close();
+    }
+  };
+
+  const instance = basicLightbox.create(
+    `
+      <img src="${event.target.dataset.source}" width="800" height="600">
+    `,
+    {
+      onShow: () => {
+        window.addEventListener('keydown', onCloseModal);
+      },
+
+      onclose: () => {
+        window.removeEventListener('keydown', onCloseModal);
+      },
+    }
+  );
+  instance.show();
+}
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+const galerryMarkup = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class='gallery__item'>
+      <a class='gallery__link' href='${original}'>
+      <img
+        class='gallery__image'
+        src='${preview}'
+        alt='${description}'/>
+      </a>
+      </li>`
+);
+
+gallery.insertAdjacentHTML('beforeend', galerryMarkup.join(''));
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
 console.log(galleryItems);
